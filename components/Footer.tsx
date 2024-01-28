20d71293425f524811f1923690466bdaac63507e
faisalimtiaz01 <129623748+faisalimtiaz01@users.noreply.github.com>
2024-01-29 03:14:49 +0500

    refractor: new changes

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,7 +5,7 @@ import React from "react";
 const Footer = () => {
   return (
     <section className="w-full footer ">
-      <main className="  bg-[#fff] rounded-b-[59px]">
+      <main className="  bg-[#fff] rounded-b-[59px] py-[45px] gap-10">
         <div className="flex justify-between max-w-[80rem] mx-auto">
           <div className="flex justify-center items-center gap-10">
             <h1 className="text-[25.223px] font-[700]"> <span className="pixel">Pixel</span><span className="tech">tect</span></h1>
@@ -14,9 +14,11 @@ const Footer = () => {
               <p className="text-[20px] text-[#323232] font-[400] leading-normal">
                 Pixeltect is headquartered in <br className="md:block hidden"/> Atlanta, Georgia.
               </p> 
+              
             </div>
+            
           </div>
-          <div className="flex gap-9 ">
+          <div className="flex gap-9  max-w-[80rem] mx-auto ">
             <div className="">
               <h5 className="text-[10.495px] text-[#323232] font-[500] leading-normal">Latest projects</h5>
               <h5 className="text-[10.495px] text-[#323232] font-[500] leading-normal">Pricing</h5>
@@ -30,13 +32,14 @@ const Footer = () => {
 
             </div>
           </div>
+          
         </div>
-
-        <div className="flex gap-5">
+        <div className="flex gap-5  max-w-[80rem] mx-auto">
             <Facebook />
             <Twitter />
          <Youtube/>
         </div>
+       
       </main>
   <div className="flex justify-between items-center max-w-[80rem] mx-auto px-12 py-[30px]">
       <p className="text-[10.495px] text-[#FAFAFA] font-[400] leading-normal">© Copyright by  PixelTect - All right reserved.</p>
